ba4d8c97436d92014d29fbad8af4937f69123cc0
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-09-22 15:25:29 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "923124533358"
+global.ownername = "Malik Sahab"
+global.ytname = "YT: Teachnical Malik Offical"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = 92124533358'  //creator number
+global.ownername = 'Malik Sahab' //owner name
+global.botname = 'MALIK-MD-BUG-BOT' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +92124533358'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaN8WMOHFxP0SLAKKu0P'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
